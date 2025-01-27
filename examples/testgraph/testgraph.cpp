998014a7832a7e6ee31e257a9bb5271996758ec5
Subhajit Sahu <wolfram77@gmail.com>
2025-01-27 21:38:53 +0530

    :bug: with experiment

--- examples/testgraph/testgraph.cpp
+++ examples/testgraph/testgraph.cpp
@@ -1,102 +1,162 @@
 #include "stdafx.h"
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <vector>
 
-int main(int argc, char* argv[]) {
-  //// what type of graph do you want to use?
-  typedef PUNGraph PGraph; // undirected graph
-  //typedef PNGraph PGraph;  //   directed graph
-  //typedef PNEGraph PGraph;  //   directed multigraph
-  //typedef TPt<TNodeNet<TInt> > PGraph;
-  //typedef TPt<TNodeEdgeNet<TInt, TInt> > PGraph;
+using namespace std;
 
-  // this code is independent of what particular graph implementation/type we use
-  printf("Creating graph:\n");
-  PGraph G = PGraph::TObj::New();
-  for (int n = 0; n < 10; n++) {
-    G->AddNode(); // if no parameter is given, node ids are 0,1,...,9
-  }
-  G->AddEdge(0, 1);
-  for (int e = 0; e < 10; e++) {
-    const int NId1 = G->GetRndNId();
-    const int NId2 = G->GetRndNId();
-    if (G->AddEdge(NId1, NId2) != -2) {
-      printf("  Edge %d -- %d added\n", NId1,  NId2); }
-    else {
-      printf("  Edge %d -- %d already exists\n", NId1, NId2); }
-  }
-  IAssert(G->IsOk());
-  //G->Dump();
-  // delete
-  PGraph::TObj::TNodeI NI = G->GetNI(0);
-  printf("Delete edge %d -- %d\n", NI.GetId(), NI.GetOutNId(0));
-  G->DelEdge(NI.GetId(), NI.GetOutNId(0));
-  const int RndNId = G->GetRndNId();
-  printf("Delete node %d\n", RndNId);
-  G->DelNode(RndNId);
-  IAssert(G->IsOk());
-  // dump the graph
-  printf("Graph (%d, %d)\n", G->GetNodes(), G->GetEdges());
-  for (PGraph::TObj::TNodeI NI = G->BegNI(); NI < G->EndNI(); NI++) {
-    printf("  %d: ", NI.GetId());
-    for (int e = 0; e < NI.GetDeg(); e++) {
-      printf(" %d", NI.GetNbrNId(e)); }
-    printf("\n");
-  }
-  // dump subgraph
-  TIntV NIdV;
-  for (PGraph::TObj::TNodeI NI = G->BegNI(); NI < G->EndNI(); NI++) {
-    if (NIdV.Len() < G->GetNodes()/2) { NIdV.Add(NI.GetId()); }
-  }
-  PGraph SubG = TSnap::GetSubGraph(G, NIdV);
-  //SubG->Dump();
-  // get UNGraph
-  { PUNGraph UNG = TSnap::ConvertGraph<PUNGraph>(SubG);
-  UNG->Dump();
-  IAssert(UNG->IsOk());
-  TSnap::ConvertSubGraph<PNGraph>(G, NIdV)->Dump(); }
-  // get NGraph
-  { PNGraph NG = TSnap::ConvertGraph<PNGraph>(SubG);
-  NG->Dump();
-  IAssert(NG->IsOk());
-  TSnap::ConvertSubGraph<PNGraph>(G, NIdV)->Dump(); }
-  // get NEGraph
-  { PNEGraph NEG = TSnap::ConvertGraph<PNEGraph>(SubG);
-  NEG->Dump();
-  IAssert(NEG->IsOk());
-  TSnap::ConvertSubGraph<PNGraph>(G, NIdV)->Dump(); }
 
-  TSnap::TestAnf<PUNGraph>();
-  return 0;
+// Type definitions.
+// Directed graph.
+typedef PNGraph PGraph;
+
+
+// Represents an edge.
+struct Edge {
+  int u, v;
+  Edge(int u, int v) : u(u), v(v) {}
+};
+
+
+
+
+// Get current time.
+inline clock_t timeNow() {
+  return clock();
 }
 
-void TestEigSvd() {
-  PNGraph G = TSnap::GenRndGnm<PNGraph>(100,1000, true);
-  PUNGraph UG = TSnap::ConvertGraph<PUNGraph>(G);
 
-  TSnap::SaveMatlabSparseMtx(G, "test1.mtx");
-  TSnap::SaveMatlabSparseMtx(UG, "test2.mtx");
+// Get time duration, in milliseconds.
+inline float duration(clock_t start, clock_t end) {
+  return 1000.0 * (end - start) / CLOCKS_PER_SEC;
+}
 
-  TFltV SngValV; TVec<TFltV> LeftV, RightV;
-  TSnap::GetSngVec(G, 20, SngValV, LeftV, RightV);
-  printf("Singular Values:\n");
-  for (int i =0; i < SngValV.Len(); i++) {
-    printf("%d\t%f\n", i, SngValV[i]()); }
-  printf("LEFT Singular Vectors:\n");
-  for (int i=0; i < LeftV[0].Len(); i++) {
-    printf("%d\t%f\t%f\t%f\t%f\t%f\n", i, LeftV[0][i](), LeftV[1][i](), LeftV[2][i](), LeftV[3][i](), LeftV[4][i]());
-  }
-  printf("RIGHT Singular Vectors:\n");
-  for (int i=0; i < RightV[0].Len(); i++) {
-    printf("%d\t%f\t%f\t%f\t%f\t%f\n", i, RightV[0][i](), RightV[1][i](), RightV[2][i](), RightV[3][i](), RightV[4][i]());
-  }
-  TFltV EigValV;
-  TVec<TFltV> EigV;
-  TSnap::GetEigVec(UG, 20, EigValV, EigV);
-  printf("Eigen Values:\n");
-  for (int i =0; i < EigValV.Len(); i++) {
-    printf("%d\t%f\n", i, EigValV[i]()); }
-  printf("Eigen Vectors %d:\n", EigV.Len());
-  for (int i =0; i < EigV[0].Len(); i++) {
-    printf("%d\t%f\t%f\t%f\t%f\t%f\n", i, EigV[0][i](), EigV[1][i](), EigV[2][i](), EigV[3][i](), EigV[4][i]());
-  }
 
+// Get random integer in the range [begin, end].
+inline int randomInt(int begin, int end) {
+  unsigned int r0 = rand() & 0xFFFF;
+  unsigned int r1 = rand() & 0xFFFF;
+  unsigned int r = (r0 << 16) | r1;
+  return begin + r % (end + 1 - begin);
+}
+
+
+// Generate edge deletions.
+inline vector<Edge> generateEdgeDeletions(PGraph x, int batchSize, bool isSymmetric) {
+  int retries = 5;
+  int n = x->GetNodes();
+  vector<Edge> deletions;
+  for (int b=0; b<batchSize; ++b) {
+    for (int r=0; r<retries; ++r) {
+      int u = randomInt(1, n);
+      TNGraph::TNodeI it = x->GetNI(u);
+      int degree = it.GetOutDeg();
+      if (degree == 0) continue;
+      int j = randomInt(0, degree-1);
+      int v = it.GetOutNId(j);
+      deletions.push_back({u, v});
+      if (isSymmetric) deletions.push_back({v, u});
+      break;
+    }
+  }
+  return deletions;
+}
+
+
+// Generate edge insertions.
+inline vector<Edge> generateEdgeInsertions(PGraph x, int batchSize, bool isSymmetric) {
+  int retries = 5;
+  int n = x->GetNodes();
+  vector<Edge> insertions;
+  for (int b=0; b<batchSize; ++b) {
+    for (int r=0; r<retries; ++r) {
+      int u = randomInt(1, n);
+      int v = randomInt(1, n);
+      if (x->IsEdge(u, v)) continue;
+      insertions.push_back({u, v});
+      if (isSymmetric) insertions.push_back({v, u});
+      break;
+    }
+  }
+  return insertions;
+}
+
+
+// Main function.
+int main(int argc, char* argv[]) {
+  char *file = argv[1];
+  bool symmetric = argc>2? atoi(argv[2]) : false;
+  bool weighted  = argc>3? atoi(argv[3]) : false;
+  int  rows      = argc>4? atoi(argv[4]) : 0;
+  int  size      = argc>5? atoi(argv[5]) : 0;
+  // Load graph from Edgelist file.
+  printf("Loading graph %s [nodes=%d, edges=%d] ...\n", file, rows, size);
+  clock_t t0 = timeNow();
+  PGraph x = TSnap::LoadEdgeList<PGraph>(file, 0, 1);
+  for (int i=1; i<=rows; ++i) {
+    if (x->IsNode(i)) continue;
+    x->AddNodeUnchecked(i);
+  }
+  clock_t t1 = timeNow();
+  printf("Nodes: %d, Edges: %d\n", x->GetNodes(), x->GetEdges());
+  printf("Elapsed time: %.2f ms\n", duration(t0, t1));
+  printf("\n");
+  // Perform batch updates of varying sizes.
+  for (int batchPower=-7; batchPower<=-1; ++batchPower) {
+    int m = x->GetEdges();
+    double batchFraction = pow(10, batchPower);
+    int batchSize = int(round(batchFraction * m));
+    printf("Batch fraction: %.1e [%d edges]\n", batchFraction, batchSize);
+    // Perform edge deletions.
+    {
+      vector<Edge> deletions = generateEdgeDeletions(x, batchSize, symmetric);
+      printf("Cloning graph ...\n");
+      clock_t t0 = timeNow();
+      PGraph y = TSnap::ConvertGraph<PGraph>(x);
+      clock_t t1 = timeNow();
+      printf("Nodes: %d, Edges: %d\n", y->GetNodes(), y->GetEdges());
+      printf("Elapsed time: %.2f ms\n", duration(t0, t1));
+      printf("Deleting edges [%zu edges] ...\n", deletions.size());
+      clock_t t2 = timeNow();
+      for (int i=0; i<deletions.size(); ++i) {
+        Edge e = deletions[i];
+        y->DelEdge(e.u, e.v);
+      }
+      clock_t t3 = timeNow();
+      printf("Nodes: %d, Edges: %d\n", y->GetNodes(), y->GetEdges());
+      printf("Elapsed time: %.2f ms\n", duration(t2, t3));
+      for (int i=0; i<deletions.size(); ++i) {
+        Edge e = deletions[i];
+        Assert(!y->IsEdge(e.u, e.v));
+      }
+    }
+    // Perform edge insertions.
+    {
+      vector<Edge> insertions = generateEdgeInsertions(x, batchSize, symmetric);
+      printf("Cloning graph ...\n");
+      clock_t t0 = timeNow();
+      PGraph y = TSnap::ConvertGraph<PGraph>(x);
+      clock_t t1 = timeNow();
+      printf("Nodes: %d, Edges: %d\n", y->GetNodes(), y->GetEdges());
+      printf("Elapsed time: %.2f ms\n", duration(t0, t1));
+      printf("Inserting edges [%zu edges] ...\n", insertions.size());
+      clock_t t2 = timeNow();
+      for (int i=0; i<insertions.size(); ++i) {
+        Edge e = insertions[i];
+        y->AddEdge(e.u, e.v);
+      }
+      clock_t t3 = timeNow();
+      printf("Nodes: %d, Edges: %d\n", y->GetNodes(), y->GetEdges());
+      printf("Elapsed time: %.2f ms\n", duration(t2, t3));
+      for (int i=0; i<insertions.size(); ++i) {
+        Edge e = insertions[i];
+        Assert(y->IsEdge(e.u, e.v));
+      }
+    }
+    printf("\n");
+  }
+  printf("\n");
+  return 0;
 }
